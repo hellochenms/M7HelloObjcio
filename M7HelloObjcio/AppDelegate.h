408258b9fa7424256729572258b28d9b54563700
M2 <iami10buxi@sina.com>
2014-07-01 17:10:01 +0800

    @主结构；

---
+++ M7HelloObjcio/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  M7HelloObjcio
+//
+//  Created by Chen Meisong on 14-7-1.
+//  Copyright (c) 2014年 chenmsm2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ M7HelloObjcio/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  M7HelloObjcio
+//
+//  Created by Chen Meisong on 14-7-1.
+//  Copyright (c) 2014年 chenmsm2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
