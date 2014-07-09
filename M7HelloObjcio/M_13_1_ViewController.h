36dad6eaff3607e5ea4b6aff8e73fd724dd14972
M2 <iami10buxi@sina.com>
2014-07-09 11:38:31 +0800

    @13-1对MMVM示例的备注；

--- M7HelloObjcio/M_13_1_ViewController.h
+++ M7HelloObjcio/M_13_1_ViewController.h
@@ -6,6 +6,10 @@
 //  Copyright (c) 2014年 chenmsm2. All rights reserved.
 //
 
+//  事实上本例并未使用真正的MMVM，
+//  真正的MMVM中，viewController是不会接触Model的，其只和ViewModel交流；
+//  本例只是借鉴MMVM思路，建立了一层伪ViewModel，来将代码分离到不同的类中；
+
 #import <UIKit/UIKit.h>
 
 @interface M_13_1_ViewController : UIViewController
